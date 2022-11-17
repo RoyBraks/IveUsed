fca67e0bd5001c275e4b9d2d147756b034416005
RoyBraks <roybraks@gmail.com>
2022-11-17 15:52:27 +0100

    Initial branch. Added first parts settings page

--- dist/js/app.js
+++ dist/js/app.js
@@ -17693,13 +17693,13 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var _navigation_vue_vue_type_template_id_cc1bf88e__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./navigation.vue?vue&type=template&id=cc1bf88e */ "./templates/components/navigation.vue?vue&type=template&id=cc1bf88e");
 /* harmony import */ var _navigation_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./navigation.vue?vue&type=script&lang=js */ "./templates/components/navigation.vue?vue&type=script&lang=js");
-/* harmony import */ var _Applications_XAMPP_xamppfiles_htdocs_education_semester_3_s3_iveused_pwa_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./node_modules/vue-loader/dist/exportHelper.js */ "./node_modules/vue-loader/dist/exportHelper.js");
+/* harmony import */ var H_XAMPP_htdocs_s3_iveused_pwa_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./node_modules/vue-loader/dist/exportHelper.js */ "./node_modules/vue-loader/dist/exportHelper.js");
 
 
 
 
 ;
-const __exports__ = /*#__PURE__*/(0,_Applications_XAMPP_xamppfiles_htdocs_education_semester_3_s3_iveused_pwa_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__["default"])(_navigation_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__["default"], [['render',_navigation_vue_vue_type_template_id_cc1bf88e__WEBPACK_IMPORTED_MODULE_0__.render],['__file',"templates/components/navigation.vue"]])
+const __exports__ = /*#__PURE__*/(0,H_XAMPP_htdocs_s3_iveused_pwa_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__["default"])(_navigation_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__["default"], [['render',_navigation_vue_vue_type_template_id_cc1bf88e__WEBPACK_IMPORTED_MODULE_0__.render],['__file',"templates/components/navigation.vue"]])
 /* hot reload */
 if (false) {}
 
