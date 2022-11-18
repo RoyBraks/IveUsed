222df6439a730a126e023919a020d59c6c1af39c
rubenvdven01 <rubenvandeven01@gmail.com>
2022-11-18 11:11:57 +0100

    Add incident map trough Leaflet with Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,13 +2,13 @@ import { createApp } from 'vue'
 
 // Import components
 import navigation from "../../templates/components/navigation";
-
-
+import incidentMap from "../../templates/components/incident-map";
 
 createApp({
     delimiters: ["[[", "]]"],
     components: {
-        "Navigation": navigation
+        "navigation": navigation,
+        "incidentmap": incidentMap
     },
     methods: {
         addSubstance (event) {
---
+++ templates/components/incident-map.vue
@@ -0,0 +1,22 @@
+<template>
+    <div ref="map">
+        
+    </div>
+</template>
+
+<script>
+    import L from 'leaflet';
+    import 'leaflet/dist/leaflet.css';
+
+    export default {
+        mounted () {
+            var mapElement = this.$refs.map;
+            mapElement.style.height = document.getElementsByClassName("map__wrapper")[0].offsetHeight + "px";
+            var leafletMap = L.map(mapElement).setView([51.505, -0.09], 13);
+
+            L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(leafletMap);
+        }
+    }
+</script>
