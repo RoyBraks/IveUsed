f973ed11f7188ace2f235eb99fc076fbffa12351
RoyBraks <roybraks@gmail.com>
2022-11-23 13:09:22 +0100

    vue fout switchknob

---
+++ templates/components/switchknob.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="switch">
+        <p>Nee</p>
+            <div class="switch__button">
+                <div class="switch__button__hole"></div>
+
+                <div class="switch__button__ball"></div>
+            </div>
+        <p>Ja</p>
+    </div>
+</template>
