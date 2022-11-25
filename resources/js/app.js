580387f0a5899d835a98760c74baee7e12a95051
rubenvdven01 <rubenvandeven01@gmail.com>
2022-11-25 10:19:24 +0100

    Dark mode test

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,17 +4,32 @@ import { createApp } from 'vue'
 import navigation from "../../templates/components/navigation";
 import incidentMap from "../../templates/components/incident-map";
 import switchknob from "../../templates/components/switchknob";
+import popup from "../../templates/components/popup";
 
 createApp({
+    data () {
+        return {
+            darkMode: false
+        }
+    },
     delimiters: ["[[", "]]"],
     components: {
+        "popup": popup,
         "navigation": navigation,
         "incidentmap": incidentMap,
         "switchknob": switchknob
     },
     methods: {
         addSubstance (event) {
-        
+            this.darkMode = !this.darkMode
+        }
+    },
+    mounted () {
+        if (this.darkMode == true) document.body.classList.add("dark-mode");
+    },
+    watch: {
+        'darkMode' (newValue) {
+            newValue ? document.body.classList.add("dark-mode") : document.body.classList.remove("dark-mode")
         }
     }
 }).mount('#app')
